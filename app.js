a19f12bff34ee93c82a440387f49769e5b987eec
Aditya Srivatsan <adityasrivatsan@Adityas-MacBook-Pro.local>
2016-06-06 00:46:48 -0400

    first changes

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ var express = require("express");
    res.send('Error, wrong validation token');
  })
 
+ app.post('/webhook/', function (req, res) {
+   messaging_events = req.body.entry[0].messaging;
+   for (i = 0; i < messaging_events.length; i++) {
+     event = req.body.entry[0].messaging[i];
+     sender = event.sender.id;
+     if (event.message && event.message.text) {
+       text = event.message.text;
+       // Handle a text message from this sender
+     }
+   }
+   res.sendStatus(200);
+ });
 
  app.listen(port);
  console.log("listening on port " + port);
