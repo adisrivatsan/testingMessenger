4b6c8d340f97ae3d995bedf5ceaf8d6b781b7457
Aditya Srivatsan <adityasrivatsan@Adityas-MacBook-Pro.local>
2016-06-05 23:16:39 -0400

    first changes

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ var express = require("express");
  var app = express();
  var port = process.env.PORT || 3000;
  app.use(express.static(__dirname));
- console.log(__dirname);
+
 
  app.get("/", function (req,res) {
  	res.sendFile(__dirname + '/index.html');
  })
-
-
+ 
  app.listen(port);
  console.log("listening on port " + port);
