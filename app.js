444d08211494611fda79fa873396a9f34c0d5067
Aditya Srivatsan <adityasrivatsan@Adityas-MacBook-Pro.local>
2016-06-11 11:22:21 -0400

    welcome screen

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ var recieptTest = {
   }
 };
 
-var welcomeMessage = function() {
+/*var welcomeMessage = function() {
   var options = {
   url: 'https://graph.facebook.com/v2.6/me/messages?access_token=EAADwfLzJvdoBAHCy4whhMSmljNMKZBWt1q785KOLcQcAOKCWRc0qaiGnCm4t8bSwYxVwMtDP5owoKiA1QjtKT2ZBdg9jx1yBRnDYhBD2nB0B0XSzIOaQQ4krjxm20VaQZAwb0LRTPZCS2H54DPK8XINYwHhF4lok1cVr5Yr3fAZDZD',
   method: 'POST',
@@ -94,7 +94,7 @@ var welcomeMessage = function() {
 request(options,function(error,body,response){
   console.log(error);
 })
-}
+} */
  //welcomeMessage();
 
 
