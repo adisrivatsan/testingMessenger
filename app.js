8bf4b1830df851961f4acf5fb0a301b45ff6b84b
Aditya Srivatsan <adityasrivatsan@Adityas-MacBook-Pro.local>
2016-06-13 16:50:49 -0400

    welcome screen

--- app.js
+++ app.js
@@ -4,10 +4,24 @@ var express = require("express");
  var bodyParser = require('body-parser');
  var request = require('request');
  var fs = require('fs');
-
+ var pg = require('pg');
  app.use(express.static(__dirname));
 
  app.use(bodyParser.json());
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
+
+
 
 
  app.get("/", function (req,res) {
