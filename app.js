209098d4fca7ecadc2627436a2c023e008711c16
Aditya Srivatsan <adityasrivatsan@Adityas-MacBook-Pro.local>
2016-06-13 01:03:53 -0400

    welcome screen

--- app.js
+++ app.js
@@ -15,12 +15,13 @@ var express = require("express");
  })
 
  app.get("/read", function (req,res) {
+   console.log(req.body + 'initial'); 
    var prevText = fs.readFileSync("sample.txt", "UTF-8");
    fs.writeFileSync("sample.txt",prevText + req.body);
    console.log(req.body);
    var sampleText = fs.readFileSync("sample.txt", "UTF-8");
    console.log('yo yo yo yo ' + sampleText);
-   res.send('working');
+   //res.send('working');
  })
 
  app.get('/webhook/', function (req, res) {
