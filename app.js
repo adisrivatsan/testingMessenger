a86cdee2eab0f060f9d0857dd4932094a15366c9
Aditya Srivatsan <adityasrivatsan@Adityas-MacBook-Pro.local>
2016-06-18 22:40:34 -0400

    welcome screen

--- app.js
+++ app.js
@@ -102,9 +102,9 @@ db.once('open', function callback () {
             testView(sender, introView);
           } else if(specificFoodTruck(text)){
               var bundle = singleFoodTruck(text,'http://static1.squarespace.com/static/530440fee4b0c7c348bab85a/t/538ff27fe4b00e487bcaaab6/1401942655441/');
+              testView(sender, bundle[0]).then(testView(sender,bundle[1]));
 
-              testView(sender,bundle[1]);
-              testView(sender, bundle[0]); 
+
           }
 
         }
