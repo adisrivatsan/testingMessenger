25a1b1149c33ca19aa2bf34b42773171c1320985
Aditya Srivatsan <adityasrivatsan@Adityas-MacBook-Pro.local>
2016-06-19 16:08:30 -0400

    welcome screen

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ db.once('open', function callback () {
 
     app.post('/webhook/', function (req, res) {
       var messaging_events = req.body.entry[0].messaging;
-      console.log('This is the payload !!!!  ' + JSON.stringify(req.body));
+      console.log('This is the payload !!!!  ' + JSON.stringify(messaging_events));
 
       for (i = 0; i < messaging_events.length; i++) {
         event = messaging_events[i];
