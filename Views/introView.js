593bc668d05f4beda599455bd549c21aaa2cf2aa
Aditya Srivatsan <adityasrivatsan@Adityas-MacBook-Pro.local>
2016-06-20 22:00:24 -0400

    getting info

--- Views/introView.js
+++ Views/introView.js
@@ -6,7 +6,7 @@ var messageData = {
               "elements": [{
                   "title": 'What would you like Today?',
                   "subtitle": "Click to see our options",
-                  "image_url": 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwj11dzSgbjNAhWKcj4KHZ30D6wQjRwIBw&url=http%3A%2F%2Fwww.123rf.com%2Fclipart-vector%2Fhungry.html&bvm=bv.124817099,d.cWw&psig=AFQjCNFn5iTUMn42fMxsYJm1JHwYnPZ3bA&ust=1466560460683106',
+                  "image_url": 'http://previews.123rf.com/images/gnicolson/gnicolson1206/gnicolson120600005/14133749-hungry-man-thinking-of-food-Stock-Vector-eating.jpg',
                   "buttons": [{
                       "type": "postback",
                       "title": "Food Trucks In Area",
