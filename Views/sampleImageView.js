9c16cd3d9f307a117042efdd68cab8adac33b07a
Aditya Srivatsan <adityasrivatsan@Adityas-MacBook-Pro.local>
2016-06-26 00:11:48 -0400

    Sample Menu View

--- Views/sampleImageView.js
+++ Views/sampleImageView.js
@@ -2,7 +2,7 @@ var imageTest = {
     attachment: {
         type: "image",
         payload: {
-            url: "file:///Users/adityasrivatsan/Documents/JavaScript/testing%20properties/atom%20trail/PagePicture/foo.jpg"
+            url: "https://imagesdb.herokuapp.com/testImage"
         }
     }
 }
