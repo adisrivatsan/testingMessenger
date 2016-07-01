8b32884da808b20b68e13ba1d381feab139284cc
Aditya Srivatsan <adityasrivatsan@Adityas-MBP.fios-router.home>
2016-06-30 20:27:37 -0400

    Updated Documentation

--- Views/introView.js
+++ Views/introView.js
@@ -13,13 +13,30 @@ var messageData = {
                       "payload":"Area"
                   }, {
                       "type": "postback",
-                      "title": "Cuisine Food Truck",
+                      "title": "Sort By Cuisine",
                       "payload": "Cuisine"
                   }, {
                       "type": "postback",
                       "title": "Open Food Trucks",
                       "payload": "Open"
                   }],
+              }, {
+                "title": "Continue",
+                "subtitle": "hello",
+                "image_url": 'http://thumbs.dreamstime.com/z/hungry-munchies-fast-food-cartoon-illustration-thinking-man-30676401.jpg',
+                "buttons": [{
+                    "type": "postback",
+                    "title": "Food Trucks In Area",
+                    "payload":"Area"
+                }, {
+                    "type": "postback",
+                    "title": "Sort By Cuisine",
+                    "payload": "Cuisine"
+                }, {
+                    "type": "postback",
+                    "title": "Open Food Trucks",
+                    "payload": "Open"
+                }]
               }],
 
           }
