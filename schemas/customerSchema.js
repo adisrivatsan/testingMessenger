e427868a72b1ae8e7224717f69ed69a92f530744
modnova <rr7patel@gmail.com>
2016-07-15 00:17:37 -0400

    customerSchema updated

--- schemas/customerSchema.js
+++ schemas/customerSchema.js
@@ -1,24 +1,18 @@
 var mongoose = require('mongoose');
 var CustSchema = mongoose.Schema({
-    FirstName: String,
-    LastName: String,
-    OrderingHistory: [{}],
-    Menu: [{
-        Name: String,
-        Price: Number
+    FirstName: String, //First name of the user
+    LastName: String, //Last name of the user
+    OrderingHistory: [{ //Stores the last 20 recent items
+        FoodItem: String, //Name of the food items
+        VendorName: String, //Name of the vendor
+        Price: Number, //Price of the purchased item
+        Currency: String, //Curency used for the purchase
+        DatePurchased: String, //Date of purchase
+        TimePurchased: String, //Time of purchase
     }],
-    CuisineType: String,
-    PhoneNumber: String,
-    Email: String,
-    Registration: String,
-    Other: [String],
-    LocationAddress: String,
-    Employees: [String],
-    Website: String,
-    Popularity: Number,
-    Wifi: Boolean,
-    ImageUrl: String,
-    ZipCode: String
+    Email: String, //Email of user
+    FavoriteStops: [String], //Favorite vendor destination (saves 10 of them)
+    SenderID: String, //Specific sender address for the Facebook Bot
 });
 
 module.exports = CustSchema;
