e6e7c4e6b9279e3ea621c97b92552949a5c2921e
Aditya Srivatsan <adityasrivatsan@Adityas-MacBook-Pro.local>
2016-07-26 14:35:24 -0400

    Modify Click Flow

--- app.js
+++ app.js
@@ -224,7 +224,7 @@ Vendor.find(function (err, ven) {
           if(specification == 'Menu') {
             sendTextMessage(sender,'in menu');
             if(chosenFoodTruck) {
-              var itemMenu = _.map(select.Menu,function(ele) {
+              var itemMenu = _.map(chosenFoodTruck.Menu,function(ele) {
                 return getItemGivenID(item,ele);
               })
               var bundle = multiItemView(itemMenu,'Name','Options','http://blogs.nordstrom.com/fashion/files/2016/06/barbecue-party-recipe-ideas-full-menu-entree-side-dish-dessert-drinks-700x700.jpg');
