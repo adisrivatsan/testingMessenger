275606ec472689d6fe385256d16fc953a8e10265
Aditya Srivatsan <adityasrivatsan@Adityas-MacBook-Pro.local>
2016-07-28 17:47:36 -0400

    Modify Click Flow

--- Views/singleFoodView.js
+++ Views/singleFoodView.js
@@ -6,7 +6,7 @@ var singleFoodTruck = function (obj, imageURL) {
                 "template_type": "generic",
                 "elements": [{
                     "title": obj.VendorName,
-                    "subtitle": "Food Truck To Order",
+                    "subtitle": 'Address: ' + obj.LocationAddress,
                     "image_url": imageURL,
                     "buttons": [{
                         "type": "postback",
@@ -16,10 +16,6 @@ var singleFoodTruck = function (obj, imageURL) {
                         "type": "postback",
                         "title": "Order",
                         "payload": obj._id + "\tOrder"
-                    }, {
-                      "type" : "postback",
-                      "title" : "Address",
-                      "payload": obj._id + "\tAddress"
                     }],
                 }],
 
