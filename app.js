8413b115ffab80c914ae62eb4feb1ef064153323
Aditya Srivatsan <adityasrivatsan@Adityas-MacBook-Pro.local>
2016-08-07 11:44:02 -0400

    Updated Files

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ var getCustomerGivenSenderID = function(collection,senderID) {
  //greeting test
 
  request.post("https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAADwfLzJvdoBAGM95GFRfHevcU4LiARXDc91sUGuqTZA6EvtS0nf76gdOUOQ996nqKDrBhwVclB2Sd7IAfnbK3zoCZBrbyL1ZAZBuwmsCuqXfZACx6jGI6LRnixvNXzAP6gcIA0YHPYSWlUZCxRGDvsAcORYZAKIoLmOJYyXhpbzQZDZD").form(
-   {setting_type:'greeting',greeting:'hello welcome to fly by'}); 
+   {setting_type:'greeting',greeting:{text:'hello welcome to fly by'}}); 
 
 //importing files and Views
 var singleFoodView = require('./Views/singleFoodView');
