902deeb5c20b9fe191765c7fe5af9aca6616cbaa
Aditya Srivatsan <adityasrivatsan@gmail.com>
2016-08-16 13:14:12 -0400

    Updated Files

--- sendingMessages/templateSend.js
+++ sendingMessages/templateSend.js
@@ -56,7 +56,7 @@ var testVasync = function testasync(sender,messageJson,nextText) {
 
   }).on('response',function(response) {
     var messageData = {
-      text:text
+      text:nextText
     };
     request({
       url: 'https://graph.facebook.com/v2.6/me/messages?access_token=EAADwfLzJvdoBAHCy4whhMSmljNMKZBWt1q785KOLcQcAOKCWRc0qaiGnCm4t8bSwYxVwMtDP5owoKiA1QjtKT2ZBdg9jx1yBRnDYhBD2nB0B0XSzIOaQQ4krjxm20VaQZAwb0LRTPZCS2H54DPK8XINYwHhF4lok1cVr5Yr3fAZDZD',
